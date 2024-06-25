2e1f34f295708ce4a4f01d0b2ec44e3984e73c10
mishalz <mishal.zulfiqar2911@gmail.com>
2024-06-25 21:07:39 +0100

    completed the random forest classifier and SVM for part 2

--- PartTwo.py
+++ PartTwo.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import f1_score, classification_report
 
 def get_clean_dataframe(dataframe: pd.DataFrame):
+
     #replacing the value of Labour (Co-op) to Labour in the party column
     dataframe['party'] = dataframe['party'].replace('Labour (Co-op)','Labour')
 
@@ -22,14 +26,59 @@ def get_clean_dataframe(dataframe: pd.DataFrame):
 
     return dataframe
 
+def get_data_for_training(dataframe: pd.DataFrame,ngrams=(1,1)):
+        
+        #to have an array of the party values as the target variable
+        y = dataframe['party'].to_numpy()
+        x = dataframe['speech'].to_numpy()
+
+        #first splitting the data into train and test
+        x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3, stratify=y,random_state=99)
+
+        #initialising the vectorizer
+        vectorizer = TfidfVectorizer(stop_words="english", max_features=4000, ngram_range=ngrams)
+
+        #extracting features from the train and test dataset using the same vectorizer
+        x_train_vectors = vectorizer.fit_transform(x_train)
+        x_test_vectors = vectorizer.transform(x_test)
+
+        return x_train_vectors, y_train, x_test_vectors,y_test
+
+def train_random_forest_classifier(x_train,y_train,x_test,y_test):
+    classifier = RandomForestClassifier(n_estimators=400, class_weight='balanced')
+    classifier.fit(x_train, y_train)
+
+    y_predictions = classifier.predict(x_test)
+     
+    print('Random Forest Classification Report')
+    print(classification_report(y_test,y_predictions))
+    print(f"Macro-Average F1 Score: {f1_score(y_test, y_predictions, average='macro')}\n")
+    
+def train_SVM_classifier(x_train,y_train,x_test,y_test):
+    classifier = SVC(kernel='linear')
+    classifier.fit(x_train, y_train)
+
+    y_predictions = classifier.predict(x_test)
+     
+    print('SVM Classification Report')
+    print(classification_report(y_test,y_predictions))
+    print(f"Macro-Average F1 Score: {f1_score(y_test, y_predictions, average='macro')}\n")
 
 if __name__ == "__main__":
     dataframe = pd.read_csv('p2-texts/hansard40000.csv')
     
     #Part a
     dataframe = get_clean_dataframe(dataframe)
-    
     print(f"Shape of the dataframe: {dataframe.shape}")
 
+    #Part b
+    x_train, y_train, x_test, y_test = get_data_for_training(dataframe)
 
+    #Part c
+    train_random_forest_classifier(x_train,y_train,x_test,y_test)
+    train_SVM_classifier(x_train,y_train,x_test,y_test)
 
+    #Part d
+    x_train_with_grams, y_train_with_grams, x_test_with_grams, y_test_with_grams = get_data_for_training(dataframe,(1,3))
+    train_random_forest_classifier(x_train_with_grams, y_train_with_grams, x_test_with_grams, y_test_with_grams)
+    train_SVM_classifier(x_train_with_grams, y_train_with_grams, x_test_with_grams, y_test_with_grams)
